48bb6163f168b01c76ac048693b9291ae2a6d314
Zaid Rahman <zaidirandhawa@outlook.com>
2024-03-26 23:57:22 +0500

    Initial commit

---
+++ app/src/main/java/com/example/a1/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.a1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Set the toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Enable the back arrow on the toolbar
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+}
